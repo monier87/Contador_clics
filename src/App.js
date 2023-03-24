abd72962c45227a799bc2a35493d8e5bdac05d51
monier87 <carlos.monier87@gmail.com>
2023-03-24 14:11:54 -0400

    Ultimo Cambio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 
 import './App.css';
+import Boton from './componentes/Boton';
+import Contador from './componentes/contador';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-     <div className='freecodecamp-log'>
 
+  const [numClics, setNumClics] = useState(0);
+
+
+  const manejarClic = () => {
+    setNumClics(numClics+1);
+  }
+
+  const reiniciarContador = () => {
+    setNumClics(0);
+  }
+
+
+
+  return (
+    <div className='App'>
+     <div className='freecodecamp-logo-contenedor'>
+      <img 
+        className='freecodecamp-logo'
+        src={freeCodeCampLogo}
+        alt='Logo de FreeCodeCamp'/>
+     </div>
+     <div className='contenedor-principal'>
+      <Contador numClics={numClics} />
+      <Boton
+        texto='Clic'
+        esBotonDeClic={true}
+        manejarClic={manejarClic} />
+      <Boton
+       texto='Reiniciar'
+       manejarClic={reiniciarContador} />
      </div>
     </div>
   );
